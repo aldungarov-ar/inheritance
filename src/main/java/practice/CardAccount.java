7def52d34251ea1360dc590e6eb58ab4ecc1a798
Aldungarov Artem <shepardcmndr@gmail.com>
2022-12-27 15:12:47 +0400

    homework 11.8 done, access modifiers set

--- src/main/java/practice/CardAccount.java
+++ src/main/java/practice/CardAccount.java
@@ -3,4 +3,11 @@ package practice;
 public class CardAccount extends BankAccount {
     // не забывайте, обращаться к методам и конструкторам родителя
     // необходимо используя super, например, super.put(10D);
+
+    private static final double COMMISSION = 0.01;
+
+    @Override
+    public void take(double amountToTake) {
+        super.take(amountToTake + (amountToTake * COMMISSION));
+    }
 }
--- src/main/java/practice/DepositAccount.java
+++ src/main/java/practice/DepositAccount.java
@@ -1,5 +1,23 @@
 package practice;
 
+import java.time.LocalDate;
+
 public class DepositAccount extends BankAccount {
 
+    private static final int TAKE_RESTRICTION_MONTH = 1;
+    private LocalDate lastIncome;
+
+    @Override
+    public void put(double amountToPut) {
+        super.put(amountToPut);
+        lastIncome = LocalDate.now();
+    }
+
+    @Override
+    public void take(double amountToTake) {
+        if (lastIncome.plusMonths(TAKE_RESTRICTION_MONTH)
+                .isBefore(LocalDate.now())) {
+            super.take(amountToTake);
+        }
+    }
 }
