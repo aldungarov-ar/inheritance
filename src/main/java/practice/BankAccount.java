0a80cedc0c4d21696864aa301c312e0d2b82eda4
Aldungarov Artem <shepardcmndr@gmail.com>
2022-12-27 15:18:33 +0400

    TODO blocks and comments removed

--- src/main/java/practice/BankAccount.java
+++ src/main/java/practice/BankAccount.java
@@ -5,22 +5,16 @@ public class BankAccount {
   protected double amount = 0;
 
   public double getAmount() {
-    //TODO: реализуйте метод и удалите todo
-    // верните значение количества денег не счету
     return amount;
   }
 
   public void put(double amountToPut) {
-    //TODO: реализуйте метод и удалите todo
-    // метод зачисляет деньги на счет
     if (amountToPut > 0) {
       amount += amountToPut;
     }
   }
 
   public void take(double amountToTake) {
-    //TODO: реализуйте метод и удалите todo
-    // метод списывает деньги со счета
     if (amountToTake > 0 && amountToTake <= amount) {
       amount -= amountToTake;
     }
